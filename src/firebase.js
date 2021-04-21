e230e836c23f7b85daa7c0375c218374e77471ed
Rohit-Ingole <rvingole68@gmail.com>
2021-04-21 16:15:41 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBI7s-dQu30hohmhKgHp2VlwgD5ZsByORE",
+  authDomain: "twitter-clone-2c69c.firebaseapp.com",
+  projectId: "twitter-clone-2c69c",
+  storageBucket: "twitter-clone-2c69c.appspot.com",
+  messagingSenderId: "1027043954198",
+  appId: "1:1027043954198:web:3960f0d88c2ca04f2a2f37",
+  measurementId: "G-TRFB6T3JER",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
